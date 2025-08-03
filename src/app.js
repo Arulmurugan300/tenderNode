c66fc3653658b40c777b586e3d9329ec46b56b13
Arulmurugan <arulmurugan.m@centizen.com>
2025-08-03 22:48:45 +0530

    play routing and request handlers using post man

--- src/app.js
+++ src/app.js
@@ -2,9 +2,29 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+  res.send({firstName:"Arulmurugan",Role:"Software Engineer"})
+})
+
+app.post("/user",(req,res)=>{
+  if(req){
+    console.log("req.param",req.query);
+    res.send("success")
+  }
+})
+
+app.patch("/user",(req,res)=>{
+  res.send("patch success")
+})
+
+app.delete("/user",(req,res)=>{
+  res.send("deleted successfully")
+})
+
 app.use('/home',(req,res)=>{
   res.send("Hai Arulmurugan M")
 })
+
 app.listen(4000,()=>{
   console.log('response')
 })
