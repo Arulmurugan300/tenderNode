c87077fae1ae2f4433e0a96a6582f3e9fbb8515c
Arulmurugan <arulmurugan.m@centizen.com>
2025-08-04 23:20:11 +0530

    handler request and middleware request practice

--- src/app.js
+++ src/app.js
@@ -1,28 +1,59 @@
 const express = require('express');
 
 const app = express();
+const {userAuth} = require('./middleware.js/auth')
 
-app.get("/user",(req,res)=>{
-  res.send({firstName:"Arulmurugan",Role:"Software Engineer"})
+//type 4 
+app.use("/user",userAuth)
+
+app.use("/admin",(req,res,next)=>{
+  console.log("admin");
+  res.send("admin call success")  
 })
-
-app.post("/user",(req,res)=>{
-  if(req){
-    console.log("req.param",req.query);
-    res.send("success")
+//type 1 middleware
+app.use((req,res,next)=>{
+  if(req.query.name !== 'arul')
+    res.status(401).send("not auth");
+  else
+    next();
+},
+(req,res)=>{
+  res.send("success call")
+})
+//type 2 middleware
+app.post('/user',(req,res,next)=>{
+  if(req.query.name !== 'arul'){  
+    res.status(401).send("unautheried user");
   }
+  else
+  next();
+})
+app.use("/user",userAuth,
+(res,req)=>{
+  req.send("success msg send")
 })
 
-app.patch("/user",(req,res)=>{
-  res.send("patch success")
+//type 3
+app.use('/user',userAuth, (req,res)=>{
+  res.send("success auth")
 })
 
-app.delete("/user",(req,res)=>{
-  res.send("deleted successfully")
+//use all() //path based . not for global level
+app.all("/user",(req,res,next)=>{
+  if(req.query.name !== 'arul')
+    res.status(401).send('unauth from all()')
+  else
+    next();
+  },
+(req,res)=>{
+    res.send("succes")
 })
 
-app.use('/home',(req,res)=>{
-  res.send("Hai Arulmurugan M")
+//
+
+app.use("/user/data",userAuth,
+(res,req)=>{
+  req.send("success msg send22")
 })
 
 app.listen(4000,()=>{
