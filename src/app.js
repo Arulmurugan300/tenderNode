8ae377da21dfe919f074c649126948b4c9d984db
Arulmurugan <arulmurugan.m@centizen.com>
2025-08-10 01:34:57 +0530

    moved to all api calls moved to  v1 file. and add validation

--- src/app.js
+++ src/app.js
@@ -1,51 +1,10 @@
 const express = require('express');
-const { User } = require('./model/user')
 const app = express();
 const { connectDB } = require('./config/dataBase')
-// error handling wrong way
+const v1 = require('./routes/v1');
 app.use(express.json())
 
-app.get('/getUser', async (req, res) => {
-  if (req.query.emailId) {
-    const email = req.query.emailId;
-    
-    try {
-      const user = await User.findOne({ emailId: email });
-      console.log(user);
-      if (user) {
-        res.send(user);
-      }
-      else {
-        res.status(400).send("not matching data");
-      }
-    }
-    catch {
-      res.status(400).send("something went wrong");
-    }
-  }
-  else {
-  res.status(400).send("Invalid Input")
-}
-})
-
-
-//signup call (post call using post man)
-app.post('/signIn', async (req, res, next) => {
-  const user = req.body
-  console.log(user, "userdata");
-
-  try {
-    const newUser = new User(user);
-    console.log(newUser, "user1");
-
-    await newUser.save();
-    res.send("successfully added")
-  }
-  catch {
-    res.send("error in your request")
-  }
-})
-
+app.use('/v1', v1);
 
 connectDB().then((res) => {
   console.log("DB sync successfully...");
